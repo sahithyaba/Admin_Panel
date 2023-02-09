d328ca89f523b637956dcedfb2d36a07d7dbeec1
SAHITHYA B A <78144295+sahithyaba@users.noreply.github.com>
2023-02-09 18:31:32 +0000

    Dashboard Started

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Card.css';
+const Card = () => {
+    return(
+        <div className="Card">
+            Card
+        </div>
+    )
+};
+
+export default Card;

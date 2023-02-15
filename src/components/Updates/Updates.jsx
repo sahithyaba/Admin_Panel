a3fca8bf33a9640eb68aa77a94fed53edf8a19a9
SAHITHYA B A <78144295+sahithyaba@users.noreply.github.com>
2023-02-15 17:54:13 +0000

    RightSide started

---
+++ src/components/Updates/Updates.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Updates.css';
+const Updates = () => {
+    return (
+        <div className="Updates">
+            hi
+        </div>
+    )
+};
+
+export default Updates;
